60099a9a8e735e950ccc899d6c2b4384bd966236
Alex Robenko <arobenko@gmail.com>
2013-10-08 17:57:19 +1000

    Added GPL licence header to all files.

--- embxx/io/iosimple.h
+++ embxx/io/iosimple.h
@@ -2,7 +2,20 @@
 // Copyright 2012 (C). Alex Robenko. All rights reserved.
 //
 
-/// @file io/iosimple.h
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/// @file embxx/io/iosimple.h
 /// Simple io module.
 /// Provides an ability to put and get integral types into and from
 /// steam buffer.
