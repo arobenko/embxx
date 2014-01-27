9dd72797b469085b0a9b4d91f1a31b45a3131f6f
Alex Robenko <arobenko@gmail.com>
2014-01-28 06:50:26 +1000

    Chanes to cmake recipes. Updating prefix of the functions/macros.

--- cmake/CompilerOptionsFuncs.cmake
+++ cmake/CompilerOptionsFuncs.cmake
@@ -13,74 +13,74 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-macro (_ar_add_compiler_flags cmake_flags options)
+macro (_embxx_add_compiler_flags cmake_flags options)
     foreach (flag ${cmake_flags})
         set (new_flags ${${flag}} ${options})
         string (REPLACE ";" " " ${flag} "${new_flags}")
     endforeach ()
 endmacro ()
 
-macro (ar_add_cxx_flags)
-          _ar_add_compiler_flags ("CMAKE_CXX_FLAGS" "${ARGN}")
+macro (embxx_add_cxx_flags)
+          _embxx_add_compiler_flags ("CMAKE_CXX_FLAGS" "${ARGN}")
 endmacro ()
 
-macro (ar_add_c_flags)
-     _ar_add_compiler_flags ("CMAKE_C_FLAGS" "${ARGN}")
+macro (embxx_add_c_flags)
+     _embxx_add_compiler_flags ("CMAKE_C_FLAGS" "${ARGN}")
 endmacro ()
 
-macro (ar_add_asm_flags)
-     _ar_add_compiler_flags ("CMAKE_ASM_FLAGS" "${ARGN}")
+macro (embxx_add_asm_flags)
+     _embxx_add_compiler_flags ("CMAKE_ASM_FLAGS" "${ARGN}")
 endmacro ()
 
-macro (ar_add_c_cxx_flags)
-    ar_add_c_flags (${ARGN})
-    ar_add_cxx_flags (${ARGN})
+macro (embxx_add_c_cxx_flags)
+    embxx_add_c_flags (${ARGN})
+    embxx_add_cxx_flags (${ARGN})
 endmacro ()
 
-macro (ar_add_asm_c_cxx_flags)
-    ar_add_asm_flags (${ARGN})
-    ar_add_c_flags (${ARGN})
-    ar_add_cxx_flags (${ARGN})
+macro (embxx_add_asm_c_cxx_flags)
+    embxx_add_asm_flags (${ARGN})
+    embxx_add_c_flags (${ARGN})
+    embxx_add_cxx_flags (${ARGN})
 endmacro ()
 
-macro (ar_set_default_compiler_options)
+macro (embxx_set_default_compiler_options)
     if (UNIX)
         set (extra_c_cxx_flags 
             "-Wall"
             "-Wextra"
             "-Werror")
-        ar_add_c_cxx_flags (${extra_c_cxx_flags})
+        embxx_add_c_cxx_flags (${extra_c_cxx_flags})
         
         set (extra_cxx_flags
             "-Woverloaded-virtual"
             "-Wno-unused-local-typedefs")
        
-        ar_add_cxx_flags (${extra_cxx_flags})
+        embxx_add_cxx_flags (${extra_cxx_flags})
     endif ()
 endmacro ()
 
-macro (ar_add_cpp11_support)
+macro (embxx_add_cpp11_support)
     if (UNIX)
-        ar_add_cxx_flags("--std=c++0x")
+        embxx_add_cxx_flags("--std=c++0x")
     endif ()
 endmacro ()
 
-macro (ar_disable_exceptions)
+macro (embxx_disable_exceptions)
     if (UNIX)
-        ar_add_cxx_flags("-fno-exceptions -fno-unwind-tables")
+        embxx_add_cxx_flags("-fno-exceptions -fno-unwind-tables")
     endif ()
 endmacro ()
 
-macro (ar_disable_rtti)
+macro (embxx_disable_rtti)
     if (UNIX)
-        ar_add_cxx_flags("-fno-rtti")
+        embxx_add_cxx_flags("-fno-rtti")
     endif ()
 endmacro ()
 
-macro (ar_disable_stdlib)
+macro (embxx_disable_stdlib)
     add_definitions(-DNOSTDLIB)
     if (UNIX)
-        ar_add_c_cxx_flags("-nostdlib")
+        embxx_add_c_cxx_flags("-nostdlib")
     endif ()
 endmacro ()
 
